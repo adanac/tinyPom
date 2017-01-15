b87c1f75d4ca9214c924ca72bae846f9165f199d
adanac <adanac@sina.com>
2017-01-15 14:52:46 +0800

    add db.sql

---
+++ tiny-intf/tiny-intf-dao/src/main/resources/db/db.sql
@@ -0,0 +1,7 @@
+create database demo;
+use demo;
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8
