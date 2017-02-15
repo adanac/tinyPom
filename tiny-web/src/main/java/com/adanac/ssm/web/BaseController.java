2807ead23db637f668a6d3a82a4b7d5c8df1ae94
adanac <adanac@sina.com>
2017-02-15 17:57:35 +0800

    add cookietest

---
+++ tiny-web/src/main/java/com/adanac/ssm/web/BaseController.java
@@ -0,0 +1,21 @@
+package com.adanac.ssm.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * BaseController 继承了ResponseResult常量类，
+ * BaseController的集合了所以的Service对象，并实现自动注入
+ * BaseController包含了Logger记录器
+ * @author adanac
+ */
+public class BaseController {
+
+	// Logger
+	protected static Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+
+	/**
+	 * The below is common service
+	 */
+
+}
